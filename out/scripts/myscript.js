71a9b8c9225975a21b725328379b415a09a4dbef
heidi valles <hvalles@ocad.ca>
2015-10-27 15:11:24 -0400

    Learning landing page

--- out/scripts/myscript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#main-nav li:first-child").addClass("active");    
-
-    $('#main-nav a').click(function() {
-        $("li.active").removeClass("active");
-        $(this).closest('li').addClass("active");
-    });
-});
